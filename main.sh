d8d6c07db17585e5752a3cf7383809a3f10924b7
Your Name <you@example.com>
2024-05-23 10:43:48 +0000

    fasdfdf

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 docker build -t fastapi1 -f app/Dockerfile app/
 docker build -t script1 -f script/Dockerfile script/
 
+docker push fastapi1:latest
+docker push script1:latest
+
 kubectl apply -f deploy.yml
 
 minikube tunnel
